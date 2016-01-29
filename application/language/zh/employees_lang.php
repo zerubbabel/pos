7944a9138ed8178c6f3fc288d44479d3334cd60e
zerubbabel <imate818@gmail.com>
2016-01-30 07:41:22 +0800

    small change on lang.php

--- application/language/zh/employees_lang.php
+++ application/language/zh/employees_lang.php
@@ -1,7 +1,7 @@
 <?php 
 
 $lang["employees_basic_information"] = "员工基本资料";
-$lang["employees_cannot_be_deleted"] = "无法删除选定的僱员，选定的员工中已有销售纪录或你正在試图删除自己 :)";
+$lang["employees_cannot_be_deleted"] = "无法删除选定的僱员，选定的员工中已有销售纪录或你正在试图删除自己 :)";
 $lang["employees_confirm_delete"] = "你确定要删除所选的员工吗？";
 $lang["employees_employee"] = "员工";
 $lang["employees_error_adding_updating"] = "添加/更新员工错误";
@@ -12,17 +12,17 @@ $lang["employees_new"] = "新员工";
 $lang["employees_none_selected"] = "您还沒有选择任何员工进行删除";
 $lang["employees_one_or_multiple"] = "员工";
 $lang["employees_password"] = "密码";
-$lang["employees_password_minlength"] = "密码需为八个字元以上";
-$lang["employees_password_must_match"] = "密码與确認密码不一致";
+$lang["employees_password_minlength"] = "密码需为八个字符以上";
+$lang["employees_password_must_match"] = "密码与确认密码不一致";
 $lang["employees_password_required"] = "请输入密码";
-$lang["employees_permission_desc"] = "勾选後授予使用該模组功能";
-$lang["employees_permission_info"] = "员工權限";
-$lang["employees_repeat_password"] = "确認密码";
+$lang["employees_permission_desc"] = "勾选后授予使用该模块功能";
+$lang["employees_permission_info"] = "员工权限";
+$lang["employees_repeat_password"] = "确认密码";
 $lang["employees_successful_adding"] = "新增员工资料成功";
 $lang["employees_successful_deleted"] = "成功删除员工资料";
 $lang["employees_successful_updating"] = "成功更新员工资料";
 $lang["employees_update"] = "更新员工";
 $lang["employees_username"] = "帐号";
-$lang["employees_username_minlength"] = "帐号必需为五个字元以上";
+$lang["employees_username_minlength"] = "帐号必需为五个字符以上";
 $lang["employees_username_required"] = "帐号为必填";
 $lang["employees_subpermission_required"] = "Add at least one grant for each module";
